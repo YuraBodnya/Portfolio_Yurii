b52654d30f3f70a24d87aa3a7f4678f9b3f28f75
Yura_Bodnya <82533492+YuraBodnya@users.noreply.github.com>
2021-11-23 10:19:15 +0300

    add exem Html

---
+++ proj/exem_html/js/script.js
@@ -0,0 +1,19 @@
+      $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:50,
+    items:1,        
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:4000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
